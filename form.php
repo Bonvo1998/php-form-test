2b46a42c09cb4beadb13fcac2a1981e781f76536
Bon Vo <63612593+Bonvo1998@users.noreply.github.com>
2020-06-11 11:03:17 -0700

    Create form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+if(){//
+
+}else{//show form
+  echo 
+  '
+  <form action="">
+  <p>First Name <input type="test" name="FirstName"/></p>
+  <p><input type="submit" /></p>
+  </form>
+  ';
+}
+?>
